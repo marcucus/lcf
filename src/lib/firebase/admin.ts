508d7704a1d78d366550afb41501a39d17dbd8d1
Adrien Marques <43345482+marcucus@users.noreply.github.com>
2025-10-23 20:54:12 +0200

    add

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, cert, App } from 'firebase-admin/app';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import { getAuth, Auth } from 'firebase-admin/auth';
+import serviceAccount from "./projectcar-2ee73-firebase-adminsdk-703pj-0e04f54092.json";
 
 let adminApp: App;
 let adminDb: Firestore;
@@ -9,7 +10,7 @@ let adminAuth: Auth;
 if (getApps().length === 0) {
   try {
     adminApp = initializeApp({
-      credential: App.credential.cert(serviceAccount as any),
+      credential: cert(serviceAccount as any),
     });
     adminDb = getFirestore(adminApp);
     adminAuth = getAuth(adminApp);
